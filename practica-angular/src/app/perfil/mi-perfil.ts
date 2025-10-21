fb2f47d977504d37b6bcbfb31bc007750893e07e
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2025-10-21 08:56:44 -0500

    Componente Perfil

---
+++ practica-angular/src/app/perfil/mi-perfil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:"mi-perfil",
+    templateUrl:"./mi-perfil.html",
+    styleUrl: "./mi-perfil.css"
+})
+
+export class Perfil{
+
+}
+
