f0a3cc14f5c4daf6b6701113c9887e148064cdd6
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2025-11-01 17:34:05 -0500

    For en Array

--- practica-angular/src/app/prestamos/prestamos.ts
+++ practica-angular/src/app/prestamos/prestamos.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+
 
 @Component({
   selector: 'app-prestamos',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './prestamos.html',
   styleUrl: './prestamos.css'
 })
 export class Prestamos {
   web = "https://open.spotify.com"
   redirigir = false;
+
+  prestamosList=[
+    {id:1, monto:232,estado:"Aprobado"},
+    {id:2, monto:2022,estado:"Pendiente"},
+    {id:3, monto:4222,estado:"Aprobado"},
+    {id:4, monto:1222,estado:"Pendiente"},
+    {id:5, monto:1422,estado:"Aprobado"},
+    {id:6, monto:3422,estado:"Pendiente"}
+  ]
 }
