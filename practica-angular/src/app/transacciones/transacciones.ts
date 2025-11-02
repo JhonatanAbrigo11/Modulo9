156de87adae3a8a38bb7b03f88d76e85dc654ea2
JhonatanAbrigo11 <jhonatanabrigo2018@gmail.com>
2025-11-02 13:26:30 -0500

    ngStyle, ok

--- practica-angular/src/app/transacciones/transacciones.ts
+++ practica-angular/src/app/transacciones/transacciones.ts
@@ -1,12 +1,18 @@
-import { NgClass } from '@angular/common';
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-transacciones',
-  imports: [NgClass],
+  imports: [NgClass,NgStyle],
   templateUrl: './transacciones.html',
   styleUrl: './transacciones.css'
 })
 export class Transacciones {
   public mostrar:string= "si"
+  misEstilos={
+    'background-color': 'brown',
+    'padding': '10px',
+    'font-weight': 'bold',
+    'font-style': 'italic'
+  };
 }
